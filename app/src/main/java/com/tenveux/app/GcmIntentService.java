c3392df1e55e0a4f5f91d8fcfe02c6bd1a110a6f
SONNA Glenn <gsonna@p-212a-09.intra.gobelins.fr>
2015-03-20 15:48:39 +0100

    notification system

--- app/src/main/java/com/tenveux/app/GcmIntentService.java
+++ app/src/main/java/com/tenveux/app/GcmIntentService.java
@@ -1,6 +1,7 @@
 package com.tenveux.app;
 
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -12,6 +13,9 @@ import android.util.Log;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.tenveux.app.activities.MainActivity;
+import com.tenveux.app.activities.PropositionsActivity;
+import com.tenveux.app.activities.menu.friends.FriendSearchRequestActivity;
+import com.tenveux.app.activities.menu.friends.NetworkActivity;
 
 /**
  * Created by theGlenn on 13/03/15.
@@ -22,6 +26,8 @@ public class GcmIntentService extends IntentService {
     NotificationCompat.Builder builder;
 
     private static final String TAG = "VoilaGCM";
+
+    private static final int SERVED = 0, TAKEN = 1, FRIEND_WANNA = 2, FRIEND_ACCEPTED = 3;
 
     public GcmIntentService() {
         super("GcmIntentService");
@@ -44,28 +50,15 @@ public class GcmIntentService extends IntentService {
              * any message types you're not interested in, or that you don't
              * recognize.
              */
-            if (GoogleCloudMessaging.
-                    MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
-                sendNotification("Send error: " + extras.toString());
-            } else if (GoogleCloudMessaging.
-                    MESSAGE_TYPE_DELETED.equals(messageType)) {
-                sendNotification("Deleted messages on server: " +
-                        extras.toString());
+            if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
+                //sendNotification("Send error: " + extras.toString());
+            } else if (GoogleCloudMessaging.MESSAGE_TYPE_DELETED.equals(messageType)) {
+                //sendNotification("Deleted messages on server: " + extras.toString());
                 // If it's a regular GCM message, do some work.
-            } else if (GoogleCloudMessaging.
-                    MESSAGE_TYPE_MESSAGE.equals(messageType)) {
-                // This loop represents the service doing some work.
-                for (int i = 0; i < 5; i++) {
-                    Log.i(TAG, "Working... " + (i + 1)
-                            + "/5 @ " + SystemClock.elapsedRealtime());
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                    }
-                }
-                Log.i(TAG, "Completed work @ " + SystemClock.elapsedRealtime());
+            } else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
+
                 // Post notification of received message.
-                sendNotification("Received: " + extras.toString());
+                sendNotification(extras);
                 Log.i(TAG, "Received: " + extras.toString());
             }
         }
@@ -76,22 +69,47 @@ public class GcmIntentService extends IntentService {
     // Put the message into a notification and post it.
     // This is just one simple example of what you might choose to do with
     // a GCM message.
-    private void sendNotification(String msg) {
-        mNotificationManager = (NotificationManager)
-                this.getSystemService(Context.NOTIFICATION_SERVICE);
+    private void sendNotification(Bundle extras) {
 
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-                new Intent(this, MainActivity.class), 0);
+        int type = Integer.parseInt(extras.getString("type"));
+        String name = extras.getString("name");
+
+        String[] messages = getResources().getStringArray(R.array.notification_messages);
+        String[] titles = getResources().getStringArray(R.array.notification_messages);
+        String msg = String.format(messages[type], name);
+
+        Class activityClass = MainActivity.class;
+        switch (type) {
+            case SERVED:
+                activityClass = PropositionsActivity.class;
+                break;
+            case TAKEN:
+                activityClass = PropositionsActivity.class;
+                break;
+            case FRIEND_WANNA:
+                activityClass = FriendSearchRequestActivity.class;
+                break;
+            case FRIEND_ACCEPTED:
+                activityClass = MainActivity.class;
+                break;
+        }
+        mNotificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, activityClass), 0);
 
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
+
                         .setSmallIcon(R.mipmap.ic_launcher)
-                        .setContentTitle("GCM Notification")
+                        .setContentTitle(titles[type])
                         .setStyle(new NotificationCompat.BigTextStyle()
                                 .bigText(msg))
                         .setContentText(msg);
 
+        Notification notification = mBuilder.build();
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+
         mBuilder.setContentIntent(contentIntent);
-        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+        mNotificationManager.notify(NOTIFICATION_ID, notification);
     }
 }
